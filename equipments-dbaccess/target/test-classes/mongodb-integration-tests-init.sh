9688e86333c0f130853d76d793e9db466106da27
skioppetto <skioppetto@gmail.com>
2016-11-29 07:52:53 +0100

    Add .gitignore to root folder

--- equipments-dbaccess/target/test-classes/mongodb-integration-tests-init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker rm -f bfp-mongodb || true
-docker run  --name bfp-mongodb -p 27017:27017 -d mongo mongod
-docker cp be-free-production-isa95.init.js bfp-mongodb:/data/db
-docker exec bfp-mongodb bash -c 'mongo < /data/db/be-free-production-isa95.init.js'
