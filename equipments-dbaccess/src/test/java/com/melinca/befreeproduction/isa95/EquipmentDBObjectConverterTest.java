9830ffe0364bfe4178b4eafcca88c27d9424d647
skioppetto <skioppetto@gmail.com>
2016-11-30 23:39:58 +0100

    Fix imports

--- equipments-dbaccess/src/test/java/com/melinca/befreeproduction/isa95/EquipmentDBObjectConverterTest.java
+++ equipments-dbaccess/src/test/java/com/melinca/befreeproduction/isa95/EquipmentDBObjectConverterTest.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.melinca.befreeproduction.commons.ExtendedProperty;
 import com.melinca.befreeproduction.commons.converters.ExtendedPropertiesDBObjectConverter;
